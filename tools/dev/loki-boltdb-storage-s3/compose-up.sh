ccfed30e766bcf751667a1e8c0cbc0f788793c15
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2022-12-09 12:56:49 +0100

    Disable multi tenant.

--- tools/dev/loki-boltdb-storage-s3/compose-up.sh
+++ tools/dev/loki-boltdb-storage-s3/compose-up.sh
@@ -16,9 +16,5 @@ done
 # build loki -gcflags "all=-N -l" disables optimizations that allow for better run with combination with Delve debugger.
 CGO_ENABLED=0 GOOS=linux go build -mod=vendor -gcflags "all=-N -l" -o "${SCRIPT_DIR}/loki" "${SCRIPT_DIR}/../../../cmd/loki"
 # ## install loki driver to send logs
-docker plugin install grafana/loki-docker-driver:latest --alias loki-compose --grant-all-permissions || true
-# build the compose image
-docker-compose -f "${SCRIPT_DIR}"/docker-compose.yml build distributor
-# cleanup sources
-rm -Rf "${SRC_DEST}"
-docker-compose -f "${SCRIPT_DIR}"/docker-compose.yml up "$@"
+docker plugin install grafana/loki-docker-driver:main-c44fb2e --alias loki-compose --grant-all-permissions || true
+docker-compose -f "${SCRIPT_DIR}"/docker-compose.yml up --build "$@"
