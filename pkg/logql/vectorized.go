e402bb5fbd02d6f4286551140e8307272e088063
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2023-08-07 17:07:01 +0200

    Create selection vector.

--- pkg/logql/vectorized.go
+++ pkg/logql/vectorized.go
@@ -2,18 +2,52 @@ package logql
 
 import (
 	//"fmt"
+	"bytes"
 
 	memmem "github.com/jeschkies/go-memmem/pkg/search"
 )
 
 type Vec interface {
 	Bytes() []byte
+	Int() []int64
 }
 
 type bytesVec []byte
 
 func (b bytesVec) Bytes() []byte {
 	return b
+}
+
+func (bytesVec) Int() []int64 {
+	return nil
+}
+
+type intVec []int64
+
+func (intVec) Bytes() []byte {
+	return nil
+}
+
+func (i intVec) Int() []int64 {
+	return i
+}
+
+// NewLinesVec returns three vectors: bytes, start and end indices for each entry.
+func NewEntriesVec(data []byte) []Vec {
+	start := int64(0)
+	startIndices := make([]int64, 0)
+	endIndices := make([]int64, 0)
+	for start < int64(len(data)-1) {
+		startIndices = append(startIndices, start)
+		next := int64(bytes.IndexByte(data[start:], '\n'))
+		if next == -1 {
+			endIndices = append(endIndices, int64(len(data)))
+			break
+		}
+		start += next + 1 // jump over \n
+		endIndices = append(endIndices, start)
+	}
+	return []Vec{bytesVec(data), intVec(startIndices), intVec(endIndices)}
 }
 
 type Batch interface {
@@ -49,22 +83,50 @@ func (b *mutableBatch) WithSelection(selection []int) Batch {
 func VecFilter(input Batch, col int, needle []byte) Batch {
 	vec := input.ColVec(col)
 	haystack := vec.Bytes()
-	//startIndices := input.ColVec(1)
+	startIndices := input.ColVec(1).Int()
 	//endIndices := input.ColVec(2)
 	//selection := make([]int, len(startIndices))
 
 	start := int64(0)
+	positions := make([]int64, 0)
 	//i := 0
 	for start < int64(len(haystack)-len(needle)) {
 		//i++
 		//fmt.Printf("(%d) starting at: %d\n", i, start)
-		index := memmem.Index(haystack[start:], needle)
-		if index == -1 {
+		pos := memmem.Index(haystack[start:], needle)
+		if pos == -1 {
 			break
 		}
-		start = start + index + 1
+		positions = append(positions, start+pos)
+		start = start + pos + 1
 		//fmt.Printf("(%d) next starting at: %d\n", i, start)
 	}
 
-	return input
+	selection := make([]int, 0)
+	pos := positions[0]
+	for i := range startIndices {
+		// TODO: try to be branchless
+		if startIndices[i] <= pos && (i+1 == len(startIndices) || pos < startIndices[i+1]) {
+			selection = append(selection, i)
+			positions = positions[1:]
+			if len(positions) == 0 {
+				break
+			}
+			pos = positions[0]
+		}
+	}
+
+	return input.WithSelection(selection)
+}
+
+func bool2int(b bool) int {
+	// The compiler currently only optimizes this form.
+	// See issue 6011.
+	var i int
+	if b {
+		i = 1
+	} else {
+		i = 0
+	}
+	return i
 }
--- pkg/logql/vectorized_test.go
+++ pkg/logql/vectorized_test.go
@@ -13,6 +13,55 @@ import (
 	"github.com/grafana/loki/pkg/logql/log"
 )
 
+func Test_EntryVec(t * testing.T) {
+	haystack := []byte(`96.81.153.18 - predovic2578 [02/May/2023:10:21:20 +0000] "PATCH /extend HTTP/1.0" 502 12210
+203.48.225.9 - - [02/May/2023:10:21:20 +0000] "PATCH /back-end/facilitate/mission-critical/orchestrate HTTP/2.0" 204 15545
+254.4.4.148 - - [02/May/2023:10:21:20 +0000] "DELETE /compelling/strategic/integrated HTTP/1.0" 500 15822
+34.194.247.251 - - [02/May/2023:10:21:20 +0000] "PATCH /niches/mindshare HTTP/2.0" 405 27960
+180.103.196.205 - strosin3705 [02/May/2023:10:21:20 +0000] "PATCH /proactive/e-markets/out-of-the-box/roi HTTP/1.1" 502 27669
+20.34.120.221 - - [02/May/2023:10:21:20 +0000] "DELETE /eyeballs/expedite/proactive HTTP/1.0" 301 22049
+63.216.162.248 - keeling6124 [02/May/2023:10:21:20 +0000] "PATCH /reinvent HTTP/2.0" 501 13633
+253.252.67.36 - - [02/May/2023:10:21:20 +0000] "GET /drive/communities/reintermediate HTTP/2.0" 416 25477
+207.148.140.17 - - [02/May/2023:10:21:20 +0000] "PATCH /orchestrate/synergies/back-end HTTP/1.1" 406 20607
+226.80.242.123 - - [02/May/2023:10:21:20 +0000] "POST /synergies HTTP/2.0" 203 2523
+`)
+
+	vecs := NewEntriesVec(haystack)
+	starts := vecs[1].Int()
+	require.Equal(t, len(starts), 10)
+	require.Equal(t, string(haystack[starts[0]:starts[1]]), "96.81.153.18 - predovic2578 [02/May/2023:10:21:20 +0000] \"PATCH /extend HTTP/1.0\" 502 12210\n")
+	require.Equal(t, string(haystack[starts[1]:starts[2]]), "203.48.225.9 - - [02/May/2023:10:21:20 +0000] \"PATCH /back-end/facilitate/mission-critical/orchestrate HTTP/2.0\" 204 15545\n")
+	require.Equal(t, string(haystack[starts[2]:starts[3]]), "254.4.4.148 - - [02/May/2023:10:21:20 +0000] \"DELETE /compelling/strategic/integrated HTTP/1.0\" 500 15822\n")
+	require.Equal(t, string(haystack[starts[3]:starts[4]]), "34.194.247.251 - - [02/May/2023:10:21:20 +0000] \"PATCH /niches/mindshare HTTP/2.0\" 405 27960\n")
+	require.Equal(t, string(haystack[starts[4]:starts[5]]), "180.103.196.205 - strosin3705 [02/May/2023:10:21:20 +0000] \"PATCH /proactive/e-markets/out-of-the-box/roi HTTP/1.1\" 502 27669\n")
+	require.Equal(t, string(haystack[starts[5]:starts[6]]), "20.34.120.221 - - [02/May/2023:10:21:20 +0000] \"DELETE /eyeballs/expedite/proactive HTTP/1.0\" 301 22049\n")
+	require.Equal(t, string(haystack[starts[6]:starts[7]]), "63.216.162.248 - keeling6124 [02/May/2023:10:21:20 +0000] \"PATCH /reinvent HTTP/2.0\" 501 13633\n")
+	require.Equal(t, string(haystack[starts[7]:starts[8]]), "253.252.67.36 - - [02/May/2023:10:21:20 +0000] \"GET /drive/communities/reintermediate HTTP/2.0\" 416 25477\n")
+	require.Equal(t, string(haystack[starts[8]:starts[9]]), "207.148.140.17 - - [02/May/2023:10:21:20 +0000] \"PATCH /orchestrate/synergies/back-end HTTP/1.1\" 406 20607\n")
+	require.Equal(t, string(haystack[starts[9]:]), "226.80.242.123 - - [02/May/2023:10:21:20 +0000] \"POST /synergies HTTP/2.0\" 203 2523\n")
+
+	require.Equal(t, len(vecs[2].Int()), 10)
+}
+
+func Test_VecFilter(t *testing.T) {
+	haystack := []byte(`96.81.153.18 - predovic2578 [02/May/2023:10:21:20 +0000] "PATCH /extend HTTP/1.0" 502 12210
+203.48.225.9 - - [02/May/2023:10:21:20 +0000] "PATCH /back-end/facilitate/mission-critical/orchestrate HTTP/2.0" 204 15545
+254.4.4.148 - - [02/May/2023:10:21:20 +0000] "DELETE /compelling/strategic/integrated HTTP/1.0" 500 15822
+34.194.247.251 - - [02/May/2023:10:21:20 +0000] "PATCH /niches/mindshare HTTP/2.0" 405 27960
+180.103.196.205 - strosin3705 [02/May/2023:10:21:20 +0000] "PATCH /proactive/e-markets/out-of-the-box/roi HTTP/1.1" 502 27669
+20.34.120.221 - - [02/May/2023:10:21:20 +0000] "DELETE /eyeballs/expedite/proactive HTTP/1.0" 301 22049
+63.216.162.248 - keeling6124 [02/May/2023:10:21:20 +0000] "PATCH /reinvent HTTP/2.0" 501 13633
+253.252.67.36 - - [02/May/2023:10:21:20 +0000] "GET /drive/communities/reintermediate HTTP/2.0" 416 25477
+207.148.140.17 - - [02/May/2023:10:21:20 +0000] "PATCH /orchestrate/synergies/back-end HTTP/1.1" 406 20607
+226.80.242.123 - - [02/May/2023:10:21:20 +0000] "POST /synergies HTTP/2.0" 203 2523
+`)
+
+	vecs := NewEntriesVec(haystack)
+	batch := NewBatch(vecs)
+	updated := VecFilter(batch, 0, []byte("DELETE"))
+	require.ElementsMatch(t, updated.Selection(), []int{2, 5})
+}
+
 func Benchmark_PipelineLarge(b *testing.B) {
 	//needle := []byte(`77.47.98.232 - - [02/May/2023:10:20:14 +0000] "GET /empower/e-business/whiteboard`)
 	needle := []byte(`whiteboard`)
@@ -34,26 +83,28 @@ func Benchmark_PipelineLarge(b *testing.B) {
 		b.ResetTimer()
 		b.ReportAllocs()
 		for n := 0; n < b.N; n++ {
-			//lines := 0
 			var iterator Iter = iterImpl{
 				scanner: bufio.NewScanner(bytes.NewReader(haystack)),
-				labels: lbs,
+				labels:  lbs,
 			}
 			for iterator.Next() {
 				entry := iterator.Entry()
 				sp.Process(0, entry.line)
 				//lines += bool2int(matches)
 			}
-			//require.Equalf(b, 76416, lines, "did not find all lines at iteration %d: want(76416), got(%d)", n, lines)
 		}
 	})
 
 	b.Run("vectorized", func(b *testing.B) {
+		vecs := NewEntriesVec(haystack)
+		require.Equal(b, len(vecs[1].Int()), 5196783)
+		require.Equal(b, len(vecs[2].Int()), 5196783)
 		b.ResetTimer()
 		b.ReportAllocs()
 		for n := 0; n < b.N; n++ {
-			batch := NewBatch([]Vec{bytesVec(haystack)})
-			VecFilter(batch, 0, needle)
+			batch := NewBatch(vecs)
+			updated := VecFilter(batch, 0, needle)
+			require.Len(b, updated.Selection(), 76416)
 		}
 	})
 }
@@ -105,16 +156,4 @@ func mustFilter(f log.Filterer, err error) log.Filterer {
 		panic(err)
 	}
 	return f
-}
-
-func bool2int(b bool) int {
-	// The compiler currently only optimizes this form.
-	// See issue 6011.
-	var i int
-	if b {
-		i = 1
-	} else {
-		i = 0
-	}
-	return i
 }
