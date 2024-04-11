fdd1b6f24d6b78353a3abc283bfb5bb30bf2798d
Karsten Jeschkies <karsten.jeschkies@grafana.com>
2024-04-11 14:22:09 +0200

    Benchmark filtering once more

--- pkg/chunkenc/batch_test.go
+++ pkg/chunkenc/batch_test.go
@@ -4,6 +4,7 @@ import (
 	//"bytes"
 	"fmt"
 	"math/rand"
+	"strings"
 
 	//"strings"
 	"testing"
@@ -11,7 +12,8 @@ import (
 
 	"github.com/dustin/go-humanize"
 	"github.com/golang/snappy"
-	memmem "github.com/jeschkies/go-memmem/pkg/search"
+
+	//memmem "github.com/jeschkies/go-memmem/pkg/search"
 
 	//"github.com/pierrec/lz4/v4"
 	"github.com/stretchr/testify/require"
@@ -25,13 +27,13 @@ func BenchmarkFilter(b *testing.B) {
 	batch := &batch{}
 	r := rand.New(rand.NewSource(42))
 
-	sizes := []uint64{256 * humanize.KiByte, 512 * humanize.KiByte, 1 * humanize.MiByte, 4 * humanize.MiByte}
-	selectivity := []float64{1.0, 0.3, 0.1, 0.01}
+	sizes := []uint64{512 * humanize.KiByte, 1 * humanize.MiByte, 4 * humanize.MiByte}
+	selectivity := []float64{1.0, 0.3, 0.1, 0.01, 0.0}
 	for _, sel := range selectivity {
 		for _, size := range sizes {
 			totalBytes := uint64(0)
 			needle := "matchme"
-			needleB := []byte(needle)
+			//needleB := []byte(needle)
 			field := ""
 			// TODO: this can be improved. With too few samples we might not have any match.
 			if r.Float64() < sel {
@@ -49,40 +51,40 @@ func BenchmarkFilter(b *testing.B) {
 
 			b.ResetTimer()
 			b.ReportAllocs()
+			b.Run(fmt.Sprintf("%s-%f", humanize.Bytes(size), sel), func(b *testing.B) {
+				for n := 0; n < b.N; n++ {
+					for _, e := range entries {
+						strings.Contains(e.Line, needle)
+					}
+					b.SetBytes(int64(totalBytes))
+				}
+			})
+
 			/*
-				b.Run(fmt.Sprintf("sequential-%s-%f", humanize.Bytes(size), sel), func(b *testing.B) {
+				b.ResetTimer()
+				b.Run(fmt.Sprintf("%s-%f", humanize.Bytes(size), sel), func(b *testing.B) {
+					haystack := batch.lines
+					var offset int64
 					for n := 0; n < b.N; n++ {
-						for _, e := range entries {
-							strings.Contains(e.Line, needle)
+						i := 0
+						for i < len(batch.offsets) {
+							start := batch.offsets[i]
+							//offset := bytes.Index(haystack[start:], []byte(needle))
+							offset = memmem.Index(haystack[start:], needleB)
+							if offset == -1 {
+								break
+							}
+
+							// Find first start that is above pos.
+							// TODO: We can probably be branchless here.
+							for i < len(batch.offsets) && batch.offsets[i] <= start+int64(offset) {
+								i++
+							}
 						}
 						b.SetBytes(int64(totalBytes))
 					}
 				})
 			*/
-
-			b.ResetTimer()
-			b.Run(fmt.Sprintf("%s-%f", humanize.Bytes(size), sel), func(b *testing.B) {
-				haystack := batch.lines
-				var offset int64
-				for n := 0; n < b.N; n++ {
-					i := 0
-					for i < len(batch.offsets) {
-						start := batch.offsets[i]
-						//offset := bytes.Index(haystack[start:], []byte(needle))
-						offset = memmem.Index(haystack[start:], needleB)
-						if offset == -1 {
-							break
-						}
-
-						// Find first start that is above pos.
-						// TODO: We can probably be branchless here.
-						for i < len(batch.offsets) && batch.offsets[i] <= start+int64(offset) {
-							i++
-						}
-					}
-					b.SetBytes(int64(totalBytes))
-				}
-			})
 		}
 	}
 }
